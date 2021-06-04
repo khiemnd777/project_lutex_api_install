47d66cbea49b7fb8b2ef2ac7cb1e255c30314275
DESKTOP-FAKMJLK\User <khiemnd777@gmail.com>
2021-06-04 23:06:44 +0700

    fix lock folder source

--- init.ps1
+++ init.ps1
@@ -15,20 +15,20 @@ $gitHttps = '{0}{1}:{2}@{3}/{4}/{5}' -f $gitProtocol, $gitEmail, $gitPassword, $
 
 # git clone source as HTTPS
 git clone $gitHttps
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
+
+# clone the init.config and use this as build.config
+Copy-Item '.\init.config.json' -Destination ('.\{0}\build.config.json' -f $config.GIT_SOURCE)
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 # switch to the source directory
-Set-Location -Path $('.\' + $config.GIT_SOURCE)
-
-# clone the init.config and use this as build.config
-Copy-Item '.\..\init.config.json' -Destination '.\build.config.json'
+Set-Location -LiteralPath $('.\' + $config.GIT_SOURCE)
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 # install and run the source
 .\build.ps1
-
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 Set-Location -LiteralPath $PSScriptRoot
-
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
